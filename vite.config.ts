f34c7431b6227b3ead5b9f31472c38d019577ef2
dsfreehand <daniel.sisson@live.com>
2025-03-25 14:45:18 -0400

    Trying some nodepolyfills...

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// Import only the necessary polyfill
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/candidate-search/',
+  resolve: {
+    alias: {
+      crypto: path.resolve(path.dirname(new URL(import.meta.url).pathname), 'node_modules/crypto-browserify'),
+    },
+  },
   define: {
     'process.env.VITE_GITHUB_API_KEY': '"your-fallback-value"'
   },
   envDir: './environment',
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
 });
